262394c4c09df94331e6e44b3630d8d7b421703a
Sai Prasad <sai.prasad@rocketmail.com>
2019-01-02 23:17:36 +0530

    Passing increment and decrement projects methods using context, provider and consumer

---
+++ public/precache-manifest.e8c82497d9770fe25a6edcf033b3f735.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "96a1bd384bbb9b542712",
+    "url": "/static/js/main.96a1bd38.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "96a1bd384bbb9b542712",
+    "url": "/static/css/main.1bbb17f6.chunk.css"
+  },
+  {
+    "revision": "1a8be666e9d57cae4874d2c6769b8d3f",
+    "url": "/index.html"
+  }
+];
